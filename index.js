fff29d837d4f9fb662e017f5225134498988216b
Scott C. Reynolds <scott@scottcreynolds.com>
2016-07-12 23:28:40 -0400

    updates

--- index.js
+++ index.js
@@ -12,13 +12,16 @@ function getRepositories() {
 function displayRepositories() {
   const repos = JSON.parse(this.responseText)
   const repoList = "<ul>" + repos.map(repo => {
-    let dataRepoName = 'data-repository="' + repo.name + '"'
-    let dataUsername = 'data-username="' + repo.owner.login + '"'
-    let str = '<li><h2>' + repo.name + '</h2>'
-    str += '<a href="' + repo.html_url + '">' + repo.html_url + '</a><br>'
-    str += '<a href="#" ' + dataRepoName + ' ' + dataUsername + ' onclick="getCommits(this)">Get Commits</a><br>'
-    str += '<a href="#" ' + dataRepoName + ' ' + dataUsername + ' onclick="getBranches(this)">Get Branches</a></li>'
-    return str
+    const dataUsername = 'data-username="' + repo.owner.login + '"'
+    const dataRepoName = 'data-repository="' + repo.name + '"'
+    return(`
+          <li>
+            <h2>${repo.name}</h2>
+            <a href="${repo.html_url}">${repo.html_url}</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+          </li>`
+          )
   }).join('') + "</ul>";
   document.getElementById("repositories").innerHTML = repoList
 }
